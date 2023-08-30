fae78df9ff208b9b2451f02ec37a141e937ea831
j3553pat <jessecodedev@gmail.com>
2023-08-30 11:01:10 -0500

    added image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,29 +1,79 @@
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
 
 export default function Profile() {
-  
-  const {currentUser} = useSelector(state => state.user)
-  console.log({currentUser})
+  const { currentUser } = useSelector((state) => state.user);
+  const [image, setImage] = useState(undefined)
+  const fileRef = useRef(null);
+
+  useEffect(() => {
+    if (image) {
+      handleImageUpload(image)
+    }
+  }, [image])
+  // console.log({ currentUser });
+
+  const handleImageUpload = async (image) => {
+    const storage = getStorage(app)
+    const fileName = new Date().getTime() + image.name
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, image)
+    uploadTask.on('state_changed', (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + "% done")
+    })
+  }
 
   return (
-    <div className='p-3 max-w-lg mxauto'>
-      <h1 className='text-4xl text-center font-semibold my-8'>Profile</h1>
-      <form className='flex flex-col gap-4'>
-      <img src={currentUser.profilePicture} alt='profile' className='h-24 w-24 self-center
-        cursor-pointer rounded-full object-cover mt-2' />
-        <input defaultValue={currentUser.username} type='text' id='username' 
-        placeholder='Username' className='bg-slate-100 rounded-lg p-4'/>
-        <input defaultValue={currentUser.email} type='email' id='email' 
-        placeholder='Email' className='bg-slate-100 rounded-lg p-4'/>
-        <input type='password' id='password' 
-        placeholder='Password' className='bg-slate-100 rounded-lg p-4'/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg
-        hover:opacity-95 disabled: opacity-80'>Update Info</button>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-4xl text-center font-semibold my-8">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input type="file" hidden accept="image/*" ref={fileRef} 
+        onChange={(e)=> setImage(e.target.files[0])}/>
+        {/* allow read; 
+      allow write: if
+      request.resource.size < 2 * 1024 * 1024 && 
+      request.resource.contentType.matches('image/.*') */}
+        <img
+          src={currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center
+        cursor-pointer rounded-full object-cover mt-2"
+          onClick={() => fileRef.current.click()}
+        />
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 rounded-lg p-4"
+        />
+        <input
+          defaultValue={currentUser.email}
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 rounded-lg p-4"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 rounded-lg p-4"
+        />
+        <button
+          className="bg-slate-700 text-white p-3 rounded-lg
+        hover:opacity-95 disabled: opacity-80"
+        >
+          Update Info
+        </button>
       </form>
-      <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
     </div>
-  )
+  );
 }
