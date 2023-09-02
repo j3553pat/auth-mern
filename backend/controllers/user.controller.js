35d767f59910a98bcfc8fa289eb40059b245fe78
j3553pat <jessecodedev@gmail.com>
2023-09-02 15:28:13 -0500

    added code for updating users info

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,21 @@
+import { errorHandler } from "../utils/error.js"
+
 export const test = (req, res) => {
     res.json({
         message: "api is working"
     })
+}
+
+//update user
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id == req.params.id) {
+       
+        return next(errorHandler(401, 'you can only upate your account.'))
+    }
+    try {
+        
+    } catch (error) {
+        
+    }
 }
